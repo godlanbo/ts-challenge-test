2cf5af591e23b1d8b71a838a9b896053b01f2775
godlanbo <912300762@qq.com>
2022-01-20 00:28:18 +0800

    第二种写法 tupleToObject

--- challenges/tuple-to-obj/template.ts
+++ challenges/tuple-to-obj/template.ts
@@ -1,3 +1,8 @@
-type TupleToObject<T extends readonly (string | symbol | number)[]> = {
+type TupleToObject<T extends readonly (keyof any)[]> = {
   [P in T[number]]: P
 }
+
+// type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+//   [P in T[number]]: P
+// }
+// 两种写法
