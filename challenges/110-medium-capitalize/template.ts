bb504118a1e6b846c5e954e6c22631d18ba51390
godlanbo <912300762@qq.com>
2022-02-03 19:58:26 +0800

    第二十一天 对象类型在类型系统中的使用

---
+++ challenges/110-medium-capitalize/template.ts
@@ -0,0 +1,34 @@
+type CharMap = {
+  a: 'A'
+  b: 'B'
+  c: 'C'
+  d: 'D'
+  e: 'E'
+  f: 'F'
+  g: 'G'
+  h: 'H'
+  i: 'I'
+  j: 'J'
+  k: 'K'
+  l: 'L'
+  m: 'M'
+  n: 'N'
+  o: 'O'
+  p: 'P'
+  q: 'Q'
+  r: 'R'
+  s: 'S'
+  t: 'T'
+  u: 'U'
+  v: 'V'
+  w: 'W'
+  x: 'X'
+  y: 'Y'
+  z: 'Z'
+}
+
+type MyCapitalize<S extends string> = S extends `${infer F}${infer R}`
+  ? F extends keyof CharMap
+    ? `${CharMap[F]}${R}`
+    : S
+  : S
