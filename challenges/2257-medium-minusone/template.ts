900532186b8e8372457689c4e181c8a5790caee3
godlanbo <912300762@qq.com>
2022-02-20 00:54:44 +0800

    第三十六天 学习骚操作，通过length获取想要的数字值

---
+++ challenges/2257-medium-minusone/template.ts
@@ -0,0 +1,16 @@
+// 通过迭代数组数量，通过 length 属性来获得想要的数字，N是会走几轮迭代，Base是每次迭代得到的值，默认
+// 每次迭代 +1
+type Naive<
+  N extends string,
+  Base extends any[] = [[]],
+  M extends any[] = []
+> = `${M['length']}` extends N ? [] : [...Base, ...Naive<N, Base, [[], ...M]>]
+
+// 这里是为了防止递归过深，在做倍乘，Res 是10倍数值，同时也是结果的累计值
+type Rec<S, Res extends any[] = []> = S extends `${infer L}${infer R}`
+  ? Rec<R, [...Naive<'10', Res>, ...Naive<L>]>
+  : Res
+
+type MinusOne<T extends number> = Rec<`${T}`> extends [infer F, ...infer R]
+  ? R['length']
+  : never
