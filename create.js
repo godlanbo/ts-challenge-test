e0b26a287e9ebbef10ffd8830f6dc210cd614c11
godlanbo <912300762@qq.com>
2022-03-06 12:06:42 +0800

    更新自动获取题目脚本

--- create.js
+++ create.js
@@ -1,6 +1,29 @@
 const fs = require('fs')
 const readline = require('readline')
 const path = require('path')
+const request = require('request')
+
+const sourceRootPath =
+  'https://cdn.jsdelivr.net/gh/type-challenges/type-challenges/questions'
+const requestInstance = request.defaults({
+  baseUrl: sourceRootPath
+})
+function promiseReq(
+  url,
+  options = {
+    proxy: 'http://127.0.0.1:1080'
+  }
+) {
+  return new Promise((resolve, reject) => {
+    requestInstance.get(url, options, (err, res) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(res)
+      }
+    })
+  })
+}
 
 const rootDirPath = path.resolve(__dirname, './challenges')
 const input = readline.createInterface({
@@ -8,11 +31,16 @@ const input = readline.createInterface({
   output: process.stdout
 })
 
-
-input.question(`题目名称：`, name => {
+input.question(`题目名称：`, async name => {
   const targetDirPath = `${rootDirPath}/${name}`
-  fs.mkdirSync(targetDirPath)
-  fs.writeFileSync(`${targetDirPath}/template.ts`, '')
-  fs.writeFileSync(`${targetDirPath}/test-case.ts`, '')
+  if (!fs.existsSync(targetDirPath)) {
+    fs.mkdirSync(targetDirPath)
+  }
+
+  const { body: templateContent } = await promiseReq(`/${name}/template.ts`)
+  const { body: testContent } = await promiseReq(`/${name}/test-cases.ts`)
+
+  fs.writeFileSync(`${targetDirPath}/template.ts`, templateContent)
+  fs.writeFileSync(`${targetDirPath}/test-case.ts`, testContent)
   input.close()
 })
